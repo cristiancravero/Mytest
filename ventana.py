00b70d12c8322efb84647ae4ef38d84ef30edd75
cristiancravero <root@Aulas.unlu.edu.ar>
2018-03-15 00:13:26 -0300

    cambios

--- ventana.py
+++ ventana.py
@@ -3,6 +3,6 @@ import wx
 app = wx.App() 
 window = wx.Frame(None, title = "ventana", size = (300,200)) 
 panel = wx.Panel(window) 
-label = wx.StaticText(panel, label = "Hello World", pos = (100,50)) 
+label = wx.StaticText(panel, label = "Hola Mundo", pos = (100,50)) 
 window.Show(True) 
 app.MainLoop()
